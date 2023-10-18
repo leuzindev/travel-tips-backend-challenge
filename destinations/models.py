b84f8952a130d9b2024a5d0741e63dcdd416b3fc
leonardo <leonardoc.soares08@gmail.com>
2023-10-18 16:27:46 -0300

    feat: new column for depoiments

--- destinations/models.py
+++ destinations/models.py
@@ -3,8 +3,10 @@ from django.db import models
 
 class Destination(models.Model):
     photo = models.CharField(max_length=255)
+    photo_2 = models.CharField(max_length=255)
     name = models.CharField(max_length=30)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    meta = models.CharField(max_length=255)
+    verbose_text = models.CharField(max_length=160, blank=True, null=True)
 
     def __str__(self):
         return self.name
